1cddb9a607cf729ceeaa3c1addbbe6460a8625db
Socketlike <36399055+Socketlike@users.noreply.github.com>
2023-05-07 07:36:16 +0700

    chore(packages): bump versions
    chore: code cleanup, remove fluxDispatcher listenerr (not actually used 99% of the time)

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,37 @@
-import { Injector, Logger, common, types, webpack } from 'replugged';
-
-type GetActiveSocketAndDeviceRes = void | {
-  socket: {
-    isPremium: boolean;
-  };
-};
+import { Injector, Logger, types, webpack } from 'replugged';
 
 /* Only typing what is needed - this is not the full store */
 interface SpotifyStore extends types.RawModule {
-  getActiveSocketAndDevice: () => GetActiveSocketAndDeviceRes;
+  getActiveSocketAndDevice: () => {
+    socket: {
+      isPremium: boolean;
+    };
+  };
 }
 
 const injector = new Injector();
 let store: SpotifyStore;
-let injected = false;
-const logger = Logger.plugin('SpotifyListenAlong');
+let injected: boolean;
 
-const getStore = async (): Promise<void> => {
-  if (store) return;
-
-  store = await webpack.waitForModule<SpotifyStore>(
-    webpack.filters.byProps('getActiveSocketAndDevice'),
+const getStore = async (): Promise<boolean> =>
+  Boolean(
+    store ||
+      (store = await webpack.waitForModule<SpotifyStore>(
+        webpack.filters.byProps('getActiveSocketAndDevice'),
+      )),
   );
 
-  if (!store) throw new TypeError('Cannot get Spotify store');
-};
-
-const storeInjectHandler = async (): Promise<void> => {
-  if (injected) return;
-
-  try {
-    await getStore();
-
-    injector.after(store, 'getActiveSocketAndDevice', (_, res): GetActiveSocketAndDeviceRes => {
+const inject = async (): Promise<void> => {
+  if (!injected && (await getStore())) {
+    injector.after(store, 'getActiveSocketAndDevice', (_, res) => {
       if (res?.socket) res.socket.isPremium = true;
       return res;
     });
 
     injected = true;
-  } catch (e) {
-    logger.error('Cannot get Spotify store; will try again at Spotify state change', e);
   }
 };
 
-export async function start(): Promise<void> {
-  await storeInjectHandler();
-  common.fluxDispatcher.subscribe('SPOTIFY_PLAYER_STATE', storeInjectHandler);
-}
+export const start = inject;
 
-export async function stop(): Promise<void> {
-  common.fluxDispatcher.unsubscribe('SPOTIFY_PLAYER_STATE', storeInjectHandler);
-  injector.uninjectAll();
-}
+export const stop = (): void => injector.uninjectAll();
